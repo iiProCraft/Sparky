5ae45d29a5cb23057c358c5f1ec04b47d67f4cb8
iiProCraft <39333271+iiProCraft@users.noreply.github.com>
2021-06-13 18:32:35 +0100

    Create Sparky.java

---
+++ src/me/jumba/sparky/Sparky.java
@@ -0,0 +1,15 @@
+package me.jumba.sparky;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Sparky extends JavaPlugin {
+
+  @Override
+  public void onEnable() {
+    /*
+    * https://www.youtube.com/watch?v=IO9XlQrEt2Y
+    * watch this video first :)
+    /*
+  }
+
+}
